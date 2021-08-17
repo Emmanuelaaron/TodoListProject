18ee205e6b20bbf3f81823b09ac06cfe420cff9c
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-08-18 00:34:21 +0300

    sets up a clone that looks like part of the minimalist project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todoArray = [
+  {
+    index: 1,
+    description: 'Jogging',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'Reading a book',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Learn coding',
+    completed: false,
+  }, {
+    index: 4,
+    description: 'Preparing food',
+    completed: false,
+  },
+];
+const todos = document.getElementById('todos');
+const todoListDisplay = () => {
+  for (let i = 0; i < todoArray.length; i += 1) {
+    const myTodo = document.createElement('div');
+    myTodo.classList.add('todos');
+    const todoChecker = document.createElement('input');
+    todoChecker.type = 'checkbox';
+    todoChecker.id = 'checkbox';
+    const todoDesc = document.createElement('p');
+    todoDesc.classList.add('tododesc');
+    todoDesc.innerHTML = todoArray[i].description;
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+    myTodo.appendChild(todoChecker);
+    myTodo.appendChild(todoDesc);
 
-  return element;
-}
+    todos.appendChild(myTodo);
+  }
+};
 
-document.body.appendChild(component());
+todoListDisplay();
+
+const clearAll = () => {
+  const myDiv = document.createElement('div');
+  myDiv.classList.add('footerCard');
+  const divPara = document.createElement('p');
+  divPara.classList.add('footerText');
+  divPara.innerText = 'Clear all completed';
+  myDiv.appendChild(divPara);
+  return myDiv;
+};
+todos.appendChild(clearAll());
