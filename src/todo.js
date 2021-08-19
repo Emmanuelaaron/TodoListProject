5740ef18208443f5470febd82f4d30d75334294b
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-08-19 19:33:40 +0300

    adds provision for updating theindexes after deleting a todo

--- src/todo.js
+++ src/todo.js
@@ -13,8 +13,15 @@ const createTodo = (arr, todoInput, completed = false) => {
 const deleteTodo = (arr) => {
   if (arr.length > 0) {
     const newArr = arr.filter((todo) => todo.completed === false);
+    IndexArrangement(newArr)
     return newArr;
   }
 };
+
+const IndexArrangement = (arr) => {
+    arr.forEach((todo, todoIndex) => {
+        todo.index = todoIndex + 1
+    });
+}
 
 export { createTodo, deleteTodo };
