07a33ce7c8e18c3900af4d69f657288b704c1da6
Chike1990 <chibuikeuzoechina@gmail.com>
2021-08-26 19:11:45 +0100

    Modify deleteTodo text and mock localStorage

--- test/deleteTodo.test.js
+++ test/deleteTodo.test.js
@@ -1,11 +1,19 @@
-import { expect } from '@jest/globals';
+import { jest } from '@jest/globals';
 import deleteTodo from '../src/deleteTodo';
 import createTodo from '../src/createTodo';
+import { load, updateStorage } from '../src/storage';
+
+jest.mock('localStorage.js');
 
 describe('Delete To-do', () => {
-  const list = [];
+  let list = [];
   createTodo(list, 'coding', true);
   test('dele an element of description "Coding" and index 1 to list', () => {
     expect(deleteTodo(list).length).toBe(0);
+  });
+  test('dele an element of description "Coding" and index 1 to list', () => {
+    list = deleteTodo(list);
+    updateStorage(list);
+    expect(load().length).toBe(0);
   });
 });
